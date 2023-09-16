1f53de6d1e04e01d03a50c777cecf29477984e52
virghiu <virghiu@gmail.com>
2023-09-16 18:31:02 -0400

    fix: service key iterator

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,22 +1,14 @@
-import { services } from '../data'
-
-const Service = () => {
+const Service = ({ icon, title, text }) => {
   return (
-    <div className='section-center services-center'>
-      {services.map((item) => {
-        return (
-          <article className='service' key={item.id}>
-            <span className='service-icon'>
-              <i className={item.icon}></i>
-            </span>
-            <div className='service-info'>
-              <h4 className='service-title'>{item.title}</h4>
-              <p className='service-text'>{item.text}</p>
-            </div>
-          </article>
-        )
-      })}
-    </div>
+    <article className='service'>
+      <span className='service-icon'>
+        <i className={icon}></i>
+      </span>
+      <div className='service-info'>
+        <h4 className='service-title'>{title}</h4>
+        <p className='service-text'>{text}</p>
+      </div>
+    </article>
   )
 }
 
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,16 @@
 import Service from './Service'
 import Title from './Title'
+import { services } from '../data'
 
 const Services = () => {
   return (
     <section className='section services' id='services'>
       <Title title='our' subtitle='services' />
-
-      <Service />
+      <div className='section-center services-center'>
+        {services.map((item) => {
+          return <Service {...item} key={item.id} />
+        })}
+      </div>
     </section>
   )
 }
