e92950b66d2fb6870aa24a2422e14f72fc1f761d
virghiu <virghiu@gmail.com>
2023-09-16 17:21:07 -0400

    feat: extract service data in dynamic

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,3 +1,4 @@
+import { services } from '../data'
 import Title from './Title'
 
 const Services = () => {
@@ -6,44 +7,19 @@ const Services = () => {
       <Title title='our' subtitle='services' />
 
       <div className='section-center services-center'>
-        <article className='service'>
-          <span className='service-icon'>
-            <i className='fas fa-wallet fa-fw'></i>
-          </span>
-          <div className='service-info'>
-            <h4 className='service-title'>saving money</h4>
-            <p className='service-text'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className='service'>
-          <span className='service-icon'>
-            <i className='fas fa-tree fa-fw'></i>
-          </span>
-          <div className='service-info'>
-            <h4 className='service-title'>endless hiking</h4>
-            <p className='service-text'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className='service'>
-          <span className='service-icon'>
-            <i className='fas fa-socks fa-fw'></i>
-          </span>
-          <div className='service-info'>
-            <h4 className='service-title'>amazing comfort</h4>
-            <p className='service-text'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
+        {services.map((item) => {
+          return (
+            <article className='service' key={item.id}>
+              <span className='service-icon'>
+                <i className={item.icon}></i>
+              </span>
+              <div className='service-info'>
+                <h4 className='service-title'>{item.title}</h4>
+                <p className='service-text'>{item.text}</p>
+              </div>
+            </article>
+          )
+        })}
       </div>
     </section>
   )
